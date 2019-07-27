38b56b55c7872f479b53ad5f06bc52190af7b696
sungkuk5420 <sungkuk5420@gmail.com>
2019-07-28 01:44:21 +0900

    url 변경

--- src/passport.js
+++ src/passport.js
@@ -16,7 +16,7 @@ passport.use(
       clientID: process.env.GH_ID,
       clientSecret: process.env.GH_SECRET,
       callbackURL: process.env.PRODUCTION
-        ? `https://cryptic-shelf-55883.herokuapp.com${routes.githubCallback}`
+        ? `https://glacial-bastion-98692.herokuapp.com${routes.githubCallback}`
         : `http://localhost:4000${routes.githubCallback}`
     },
     githubLoginCallback
@@ -28,7 +28,7 @@ passport.use(
     {
       clientID: process.env.FB_ID,
       clientSecret: process.env.FB_SECRET,
-      callbackURL: `https://cryptic-shelf-55883.herokuapp.com${
+      callbackURL: `https://glacial-bastion-98692.herokuapp.com${
         routes.facebookCallback
       }`,
       profileFields: ["id", "displayName", "photos", "email"],
